8fcada06d4fcc3aeecaf974a325846a61e4ff057
Ben <bjeffrey92@hotmail.com>
2019-07-10 10:46:26 +0100

    edited write_summary_of_fits function so it writes summary of prediction accuracy as well

--- R/write_summary_of_fits.R
+++ R/write_summary_of_fits.R
@@ -30,10 +30,11 @@ write_summary_of_fits <- function(all_data_files = NULL,
 
     all_rows <- list()
     pos <- 1
+    all_validation_rows <- list()
+    validation_pos <- 1
     for (item in all_model_fits){
         result <- item[[1]][['result']]
         filename <- item[[2]]
-        validation <- item[[1]][['validation']]
      
         for (row in 1:nrow(result$summary.fixed)){
             new_row <- c(row.names(result$summary.fixed)[[row]],
@@ -49,9 +50,30 @@ write_summary_of_fits <- function(all_data_files = NULL,
             all_rows[[pos]] <- new_row
             pos <- pos + 1
         }
+
+        if ('validation' %in% names(item[[1]])){
+            validation <- item[[1]][['validation']]
+            for (i in 1:length(validation)){
+                if (length(validation[[i]])== 1){
+                    new_row <- c(names(validation)[[i]],
+                                validation[[i]],
+                                filename)
+                    all_validation_rows[[validation_pos]] <- new_row
+                    validation_pos <- validation_pos + 1
+                }
+            }
+        }        
     }
     out <- do.call('rbind', all_rows)
     out_df  <- as.data.frame(out)
     names(out_df) <- c('Parameter', names(result$summary.hyperpar), 'filename')
-    return(out_df)
+    
+    if (length(all_validation_rows) > 0){
+        validation_df <- as.data.frame(
+                                    do.call('rbind', all_validation_rows))
+        names(validation_df) <- c('Measure', 'Value', 'filename')
+        return(list(out_df, validation_df))
+    }else{
+        return(out_df)
+    }
 }
