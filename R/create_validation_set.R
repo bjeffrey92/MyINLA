1937d12fbb49b99f606476a93cecd4b5a570f738
Ben <bjeffrey92@hotmail.com>
2019-07-10 12:12:53 +0100

    created functionality to use ets forecast algorithm to predict future values of covariates

--- R/create_validation_set.R
+++ R/create_validation_set.R
@@ -13,28 +13,57 @@ create_validation_set <- function(data, time_col, location_col, response_col,
                                 covariate_cols, prediction_steps, 
                                 covariate_change='Constant'){
     
-    if (covariate_change != 'Constant'){
-        stop('Only constant covariate model is implemented currently')
-    }
-
     #splits validation data 
     last_time_points <- tail(sort(unique(data[[time_col]])), prediction_steps)
     validation_data <- data[data[[time_col]] %in% last_time_points,]
-    
-    most_recent_time <- tail(sort(unique(data[[time_col]])), 
-                            prediction_steps+1)[[1]]
 
-    for (location in unique(data[[location_col]])){
+    if (covariate_change == 'Constant'){
+        most_recent_time <- tail(sort(unique(data[[time_col]])), 
+                                prediction_steps+1)[[1]]
 
-        for (covariate in covariate_cols){
+        for (location in unique(data[[location_col]])){
 
-            #data will assume covaraiates stay the same as last recorded time point
-            most_recent_covar <- data[data[[location_col]] == location &
-                                    data[[time_col]] == most_recent_time,][[covariate]]
-            data[data[[time_col]] %in% last_time_points &
-                data[[location_col]] == location,][[covariate]] <- most_recent_covar
+            for (covariate in covariate_cols){
+
+                #data will assume covaraiates stay the same as last recorded time point
+                most_recent_covar <- data[data[[location_col]] == location &
+                                        data[[time_col]] == most_recent_time,][[covariate]]
+                data[data[[time_col]] %in% last_time_points &
+                    data[[location_col]] == location,][[covariate]] <- most_recent_covar
+            }
+            data[data[[time_col]] %in% last_time_points,][[response_col]] <- NA
         }
-        data[data[[time_col]] %in% last_time_points,][[response_col]] <- NA
+        return(list(data, validation_data))
+
+    }else if (covariate_change == 'ETS'){
+        
+        previous_times <- c()
+        for (i in unique(data[[time_col]])){
+            if (!(i %in% last_time_points)){
+                previous_times <- c(previous_times, i)
+            }
+        }
+
+        for (location in unique(data[[location_col]])){
+
+            for (covariate in covariate_cols){
+
+                previous_covars <- data[data[[location_col]] == location &
+                                    data[[time_col]] %in% previous_times,][[covariate]]
+                covar_ts <- ts(previous_covars, start = min( previous_times))
+
+                ets_prediction <- forecast::forecast(forecast::ets(covar_ts),
+                                            h=prediction_steps)
+                ets_prediction <- as.numeric(ets_prediction$mean)
+
+                for (i in 1:length(last_time_points)){
+                    data[data[[time_col]] == last_time_points[[i]] &
+                        data[[location_col]] == location,][[covariate]] <- ets_prediction[[i]]
+                }
+            }
+        }
+        return(list(data, validation_data))
+    } else{
+        stop('Unknown value of covariate_change, use Constant or ETS')
     }
-    return(list(data, validation_data))
 }
